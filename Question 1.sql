717fd0598208bfce20677d77e0d2b8e7d4d3d731
Piyush Shukla <100999352+Piyush8888@users.noreply.github.com>
2025-01-02 20:25:31 +0530

    Create Question 1.sql

---
+++ Question 1.sql
@@ -0,0 +1,6 @@
+# Question 1
+Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region.
+Ans.>>SELECT market FROM dim_customer 
+WHERE customer = 'Atliq Exclusive' AND region = 'APAC'
+GROUP BY market
+ORDER BY market ;
